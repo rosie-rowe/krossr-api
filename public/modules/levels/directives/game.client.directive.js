da4f7800468a75ea7c7dec1a27c0fafbb97962f7
Brian Rowe <admin@brian-rowe.com>
2015-01-01 01:39:32 -0500

    clear the dragbox if the user goes too far away from the game, to prevent glitches caused by leaving and re-entering the game with a dragbox present

--- public/modules/levels/directives/game.client.directive.js
+++ public/modules/levels/directives/game.client.directive.js
@@ -31,13 +31,27 @@ angular.module('levels').directive('game', [
                     }   
                 });
                 
+                // But if the user goes too far away from the game area, clear the dragbox
+                // and empty the tiles.
+                elem.on('mouseleave', function(e) {
+                    e.preventDefault();
+
+                    if (gameCtrl.dragBox && gameCtrl.dragBox.startCoord && gameCtrl.dragBox.endCoord) {
+                        var initState = gameCtrl.dragBox.initState,
+                            coords = gameCtrl.processDragBox(gameCtrl.dragBox);
+
+                        angular.forEach(coords, function(value, key) {
+                            var theTileController = gameCtrl.findTileCtrlByCoord(value);
+                            theTileController.change(value, initState, 'empty');
+                        });
+
+                        gameCtrl.clearDragBox();
+                    }     
+                });
+
                 // If a user starts dragging a tile and their mouse pointer leaves the game area,
                 // the area that was highlighted before should stay highlighted,
                 // and should activate when the user lets go of the mouse button.
-                elem.on('mouseleave', function() {
-                    
-                });
-
                 // When the mouse is released in the game, attempt to process a dragbox and check if the game is won.
                 // This event works with the mouseup event in TileController and 
                 // should always run after that event due to bubbling.
