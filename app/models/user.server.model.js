f1ad5a9ca63d0ff9ea5a5acbc92811254f2c2e0c
Brian Rowe <admin@brian-rowe.com>
2015-05-21 21:04:33 -0400

    Fixed up home screen and user profile pages a bit

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -41,7 +41,7 @@ var UserSchema = new Schema({
 	password: {
 		type: String,
 		default: '',
-		validate: [validateLocalStrategyPassword, 'Password should be longer']
+		validate: [validateLocalStrategyPassword, 'Password should be at least 10 characters']
 	},
 	salt: {
 		type: String
--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -3,6 +3,7 @@
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
 		$scope.authentication = Authentication;
+		$scope.minPasswordLength = 10;
 
 		// If user is signed in then redirect back home
 		if ($scope.authentication.user) $location.path('/levels');
