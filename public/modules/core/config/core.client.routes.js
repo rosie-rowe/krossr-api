841be54a3c04f0d6a302206071259a3c8c447427
brian <brianrowe@alumni.ecu.edu>
2015-09-03 23:35:10 -0400

    Make it possible for a user that is not signed in to play the game. Fixes #1

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -4,13 +4,13 @@
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
 		// Redirect to home view when route not found
-		$urlRouterProvider.otherwise('/');
+		$urlRouterProvider.otherwise('/levels');
 
 		// Home state routing
 		$stateProvider.
 		state('home', {
-			url: '/',
-			templateUrl: 'modules/core/views/home.client.view.html'
+			url: '/levels',
+			templateUrl: 'modules/levels/views/list-levels.client.view.html'
 		});
 	}
 ]);
