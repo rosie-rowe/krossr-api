8c09b570f800373f6536d67778f843e9c978b9b9
brian <brianrowe@alumni.ecu.edu>
2015-09-03 23:39:46 -0400

    Do show the Levels menu item to someone who isn't logged in

--- public/modules/levels/config/levels.client.config.js
+++ public/modules/levels/config/levels.client.config.js
@@ -4,6 +4,6 @@
 angular.module('levels').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Levels', 'levels', 'levels', '/levels(/create)?', false, undefined, 0, 'fa fa-folder');
+		Menus.addMenuItem('topbar', 'Levels', 'levels', 'levels', '/levels(/create)?', true, undefined, 0, 'fa fa-folder');
 	}
 ]);
