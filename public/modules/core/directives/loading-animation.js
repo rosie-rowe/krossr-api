092eb7e99ee84fa7214c49596bc17d40df47624b
brian <brianrowe@alumni.ecu.edu>
2015-10-17 00:08:41 -0400

    last checkin before tackling create/edit level

--- public/modules/core/directives/loading-animation.js
+++ public/modules/core/directives/loading-animation.js
@@ -5,7 +5,9 @@ angular.module('core').directive('loadingAnimation', ['$window', 'debounce', 'Ut
       condition: '='
     },
     template: '<div class="loadingAnimation animate-hide" k-show="condition" after-hide="afterHide()" after-show="afterShow()">' +
-                  '<div class="loadingAnimation-inner">K</div>' +
+                  '<div class="loadingAnimation-inner">' +
+                    '<span class="specialK">K</div>' +
+                  '</div>' +
               '</div>',
     link: function(scope, element) {
       scope.afterShow = function() {
