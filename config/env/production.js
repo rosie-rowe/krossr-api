c7b2420db4e59e1a834d0c19b123ea85c4ae5f80
brian <brianrowe@alumni.ecu.edu>
2016-05-03 22:32:54 -0400

    slight adjustment

--- config/env/production.js
+++ config/env/production.js
@@ -9,8 +9,12 @@ module.exports = {
 	port: process.env.PORT || 3000,
 	idappr: process.env.SERVER_ADDR || '127.0.0.1',
 	assets: {
+        lib: {
+            css: [],
+            js: ['public/dist/lib.min.js']
+        },
 		css: 'public/dist/application.min.css',
-		js: ['public/dist/lib.min.js', 'public/dist/application.min.js'],
+		js: ['public/dist/application.min.js'],
         html: 'public/dist/templates.js'
 	},
 	mailer: {
