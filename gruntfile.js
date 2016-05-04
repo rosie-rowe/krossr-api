ef87afca12f67e20126c41f8eab3e12eeb62c213
brian <brianrowe@alumni.ecu.edu>
2016-05-03 22:40:16 -0400

    another typo

--- gruntfile.js
+++ gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
 				files: {
 					'public/dist/application.min.js': '<%= applicationJavaScriptFiles %>',
                     'public/dist/lib.min.js': '<%= applicationJavaScriptLibFiles %>',
-                    'public/dist/templates.min.js', 'public/dist/templates.js'
+                    'public/dist/templates.min.js': 'public/dist/templates.js'
 				}
 			}
 		},
