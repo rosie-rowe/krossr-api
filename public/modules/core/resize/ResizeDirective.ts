7e750ca31d89fa4855bcb9beed58aabd4f74b47f
Brian Rowe <browe404@gmail.com>
2017-06-25 00:31:17 -0400

    move / typescript ResizeDirective

---
+++ public/modules/core/resize/ResizeDirective.ts
@@ -0,0 +1,37 @@
+/// <reference path="../../levels/utils/Utils.d.ts" />
+
+'use strict';
+
+class ResizeDirective implements angular.IDirective {
+    static $name = 'resize';
+
+    static $inject = [
+        '$window',
+        'debounce',
+        'Utils'
+    ]
+
+    constructor(
+        private $window: angular.IWindowService,
+        private debounce,
+        private Utils: IUtils
+    ) {
+        
+    }
+
+    link = (scope) => {
+        var gameMatrix;
+
+        angular.element(this.$window).on('resize', this.debounce(() => {
+            gameMatrix = this.Utils.getGameMatrix();
+
+            if (gameMatrix) {
+                this.Utils.calculatePlayableArea();
+                this.Utils.setGameSize(gameMatrix.length);
+                scope.$apply();
+            }
+        }, 250));
+    }
+}
+
+angular.module('core').directive(ResizeDirective.$name, ($window, debounce, Utils) => { return new ResizeDirective($window, debounce, Utils); });
