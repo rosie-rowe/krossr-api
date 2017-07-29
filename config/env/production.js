d20c442848d251f689caf315b16f2fcec53de294
Brian Rowe <browe404@gmail.com>
2017-07-29 17:32:06 -0400

    remove a lot of production-specific config

--- config/env/production.js
+++ config/env/production.js
@@ -9,11 +9,6 @@ module.exports = {
 	port: process.env.PORT || 3000,
 	idappr: process.env.SERVER_ADDR || '127.0.0.1',
 	assets: {
-        lib: {
-            css: [],
-            js: ['public/dist/vendor.bundle.js']
-        },
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/krossr.bundle.js'
+		css: 'public/dist/application.min.css'
 	}
 };
