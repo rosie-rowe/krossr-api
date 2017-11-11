079fe9690649074da5fc4ee21976d473fd796981
Brian Rowe <browe404@gmail.com>
2017-11-11 15:02:54 -0500

    While impressive, now that there is no concept of "lives" the game shouldn't automatically tell you that a line has been completed since it is too easy

--- public/modules/levels/numberLine/NumberLineController.ts
+++ public/modules/levels/numberLine/NumberLineController.ts
@@ -88,10 +88,6 @@ export class NumberLineController {
                     }
                 );
 
-                /* if a grouping's tiles all contain the correct values, we want to mark that group off in the view so that the user
-                    can keep better track of their progress */
-                this.cssClass = this.determineCssForGroup(currentGroup);
-
                 resetInd = true;
             } else {
                 if (resetInd) {
@@ -105,34 +101,6 @@ export class NumberLineController {
         return currentGroup;
     }
 
-    private determineCssForGroup(group: TileGroup): string {
-        let groupCompleted = this.isGroupCompleted(group); 
-
-        if (groupCompleted) {
-            return 'finishedGrouping';
-        } else {
-            return '';
-        }   
-    }
-
-    private isGroupCompleted(group: TileGroup): boolean {
-        let groupAsArray: string[] = Object.keys(group);
-
-        return groupAsArray.every((value, index, array) => {
-            return this.isGroupingCompleted(group[value]);
-        });
-    }
-
-    private isGroupingCompleted(grouping: TileGroup[]): boolean {
-        if (Array.isArray(grouping)) {
-            return grouping.every(function(value, index, array) {
-                return array[index].currentValue === array[index].goalValue;
-            });
-        } else {
-            return true;
-        }
-    }
-
     /* To compute the number lines for the current row or column, we need to find the length of each grouping */
     private getGroupings(currentGroup: TileGroup): LineContent[] {
         return Object.keys(currentGroup).map((value, index) => {
@@ -143,60 +111,12 @@ export class NumberLineController {
         });
     }
 
-    /* Knowing the group already exists, update the css classes on it */
-    private recalculateGroup(index: number, orientation: string): boolean {
-        /* We need to keep track if anything changed so we know whether or not to actually change lineContent,
-            because if we change it regardless we'll end up with the infdig error */
-        let changed: boolean = false;
-        let newValue;
-        let newCssClass;
-        let coord;
-        let currentGroupAsArray = Object.keys(this.currentGroup);
-        let i = 0;
-        let j;
-        let groupLen = currentGroupAsArray.length;
-        let entry;
-        let entryLen;
-        let value;
-
-        for (; i < groupLen; i++) {
-            entry = this.currentGroup[currentGroupAsArray[i]];
-            entryLen = entry.length;
-
-            for (j = 0; j < entryLen; j++) {
-                value = entry[j];
-
-                if (value.coord) {
-                    newValue = this.gameMatrix.getValueAt(value.coord.y, value.coord.x);
-
-                    if (value.currentValue !== newValue) {
-                        value.currentValue = newValue;
-                        changed = true;
-                    }
-                }
-            }
-        }   
-
-        newCssClass = this.determineCssForGroup(this.currentGroup);
-
-        if (this.cssClass !== newCssClass) {
-            this.cssClass = newCssClass;
-            changed = true;
-        }
-
-        return changed;
-    }
-
     /* For a given row or column, compute its number line (guide numbers on the sides of the board) */
     private getLineContent(): LineContent[] {
         if (!this.hasGroup) {
             this.currentGroup = this.calculateGroup(this.index, this.orientation);
             this.hasGroup = true;
             this.lineContent = this.accountForZeros(this.adjustContentForOrientation(this.getGroupings(this.currentGroup), this.orientation));
-        } else {
-            if (this.recalculateGroup(this.index, this.orientation)) {
-                this.lineContent = this.accountForZeros(this.adjustContentForOrientation(this.getGroupings(this.currentGroup), this.orientation));
-            };
         }
 
         return this.lineContent;
