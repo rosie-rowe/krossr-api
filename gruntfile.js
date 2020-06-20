81006460a619aef0b31655c6fca7dcc83d11b94d
Rosalyn Rowe <rrowe404@gmail.com>
2020-06-20 19:18:04 -0400

    load-grunt-tasks isn't needed, uninstall package

--- gruntfile.js
+++ gruntfile.js
@@ -51,9 +51,6 @@ module.exports = function(grunt) {
 		}
 	});
 
-	// Load NPM tasks
-	require('load-grunt-tasks')(grunt);
-
 	// Making grunt default to force in order not to break the project.
 	grunt.option('force', true);
 
