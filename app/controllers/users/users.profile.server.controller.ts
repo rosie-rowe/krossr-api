f913a04d1a7d03557d8463dec3bf127be7040532
Rosalyn Rowe <rrowe404@gmail.com>
2020-06-24 21:04:12 -0400

    begin writing controllers as classes...

--- app/controllers/users/users.profile.server.controller.ts
+++ app/controllers/users/users.profile.server.controller.ts
@@ -6,46 +6,52 @@
 var _ = require('lodash'),
 	errorHandler = require('../errors');
 
-/**
- * Update user details
- */
-exports.update = function(req, res) {
-	// Init Variables
-	var user = req.user;
+class UserProfileController {
+	/**
+	 * Update user details
+	 */
+	update = (req, res) => {
+		// Init Variables
+		var user = req.user;
 
-	// For security measurement we remove the roles from the req.body object
-	delete req.body.roles;
+		// For security measurement we remove the roles from the req.body object
+		delete req.body.roles;
 
-	if (user) {
-		// Merge existing user
-		user.update({
-			updated: Date.now(),
-			email: req.body.email
-		});
-
-		user.save().then(function() {
-			req.login(user, function(err) {
-				if (err) {
-					res.status(400).send(err);
-				} else {
-					res.jsonp(user);
-				}
+		if (user) {
+			// Merge existing user
+			user.update({
+				updated: Date.now(),
+				email: req.body.email
 			});
-		}).catch(function(err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
+
+			user.save().then(function () {
+				req.login(user, function (err) {
+					if (err) {
+						res.status(400).send(err);
+					} else {
+						res.jsonp(user);
+					}
+				});
+			}).catch(function (err) {
+				return res.status(400).send({
+					message: errorHandler.getErrorMessage(err)
+				});
 			});
-		});
-	} else {
-		res.status(400).send({
-			message: 'User is not signed in'
-		});
+		} else {
+			res.status(400).send({
+				message: 'User is not signed in'
+			});
+		}
 	}
-};
 
-/**
- * Send User
- */
-exports.me = function(req, res) {
-	res.jsonp(req.user || null);
-};
+	/**
+	 * Send User
+	 */
+	me = (req, res) => {
+		res.jsonp(req.user || null);
+	}
+}
+
+let me = new UserProfileController();
+exports.update = me.update;
+exports.me = me.me;
