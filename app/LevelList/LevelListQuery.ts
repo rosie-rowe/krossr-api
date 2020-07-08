7f44067f2e2709ee8ed12f3ebebbde35a18b2bde
Rosalyn Rowe <rrowe404@gmail.com>
2020-07-08 00:21:45 -0400

    use LevelListFilterOptions here too

--- app/LevelList/LevelListQuery.ts
+++ app/LevelList/LevelListQuery.ts
@@ -1,8 +1,6 @@
-export interface LevelListQuery {
+import { LevelListFilterOptions } from '@krossr/types';
+
+export interface LevelListQuery extends LevelListFilterOptions {
     pageNum: string;
     numPerPage: string;
-    sizeRestriction: string;
-    searchText: string;
-    sortBy: string;
-    sortDirection: string;
 }
