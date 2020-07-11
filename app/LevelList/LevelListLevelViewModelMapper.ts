eef6d9a12ca86a170d0a80a26e1cc6897ef653e8
Rosalyn Rowe <rrowe404@gmail.com>
2020-07-10 23:05:26 -0400

    compute prettySize on the server to reuse the formatSize method instead of reimplementing on the client

--- app/LevelList/LevelListLevelViewModelMapper.ts
+++ app/LevelList/LevelListLevelViewModelMapper.ts
@@ -1,15 +1,22 @@
 import { ViewModelMapper } from '../ViewModel/ViewModelMapper';
 import { Level } from '../models/LevelModel';
 import { LevelListLevelViewModel } from '@krossr/types';
-import { injectable } from 'inversify';
+import { injectable, inject } from 'inversify';
+import { SizeFormatter } from '../Size/SizeFormatter';
 
 @injectable()
 export class LevelListLevelViewModelMapper implements ViewModelMapper<Level, LevelListLevelViewModel> {
+    constructor(
+        @inject(SizeFormatter) private sizeFormatter: SizeFormatter
+    ) {
+    }
+
     toViewModel(model: Level): LevelListLevelViewModel {
         return {
             id: model.id,
             layout: model.layout,
             name: model.name,
+            prettySize: this.sizeFormatter.formatSize(model.size),
             size: model.size,
             avgRating: (model as any).dataValues.avgRating, // TODO
             createdAt: model.createdAt.toString(),
