d41e1928da0ad686041e62c1d5ec6c5d6567cdea
Rosalyn Rowe <rrowe404@gmail.com>
2020-07-16 19:48:45 -0400

    remove the example code and get the first app-specific test working

---
+++ app/Size/SizeFormatter.spec.ts
@@ -0,0 +1,16 @@
+import jasmine from 'jasmine';
+require('reflect-metadata');
+import { SizeFormatter } from './SizeFormatter';
+
+describe('SizeFormatter', () => {
+    let sizeFormatter;
+
+    beforeEach(() => {
+        sizeFormatter = new SizeFormatter();
+    });
+
+    it('should format the size correctly', () => {
+        let formatted = sizeFormatter.formatSize(5);
+        expect(formatted).toEqual('5x5');
+    });
+});
